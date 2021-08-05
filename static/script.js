f84996275a9d495cafde68049bf5e140ec2f2c1a
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2021-08-05 09:12:01 +0200

    Show the status of the download using colors

--- static/script.js
+++ static/script.js
@@ -1,5 +1,7 @@
 var ws = new WebSocket(`ws://${location.host}/ws`);
 ws.onmessage = function (evt) {
   const task = JSON.parse(evt.data);
-  document.getElementById(task.id).textContent = `${task.description} - ${task.progress}%`;
+  const element = document.getElementById(task.id);
+  element.textContent = `${task.description} - ${task.progress}%`;
+  element.setAttribute("class", task.status);
 };
